8f634cfd5c2ecd89c8e833a850408774de1cf071
Muhammad Afifudin <afif.x.udin@gmail.com>
2020-04-20 09:38:23 +0700

    Add header & search responsive design and animation

--- views/main.php
+++ views/main.php
@@ -22,20 +22,49 @@
 
 <body>
 
-	<header class="main-header">
-		<div class="logo-header p-0 w-100" data-background-color="red">
-			<div class="container-fluid px-lg-4 px-xl-5"><a href="" class="text-white text-decoration-none fw-7">Kode Wilayah</a></div>
+	<header class="bg-primary-gradient pb-6">
+		<nav class="p-a p-sm-3">
+			<a id="nav-brand" href="" class="d-block lh-120">
+				<i class="icon-location-pin mr-1"></i>
+				<span class="fw-8">KODE</span>
+				<span class="fw-3">WILAYAH</span>
+			</a>
+		</nav>
+		<div class="search-form-wrapper-outer">
+			<div class="search-form-wrapper">
+				<input id="search" type="search" class="search-form-input" placeholder="Cari kode atau nama wilayah" autofocus>
+				<button type="button" class="search-form-btn"><i class="icon-magnifier"></i></button>
+				<div class="search-form-setting-wrapper">
+					<button type="button" class="search-form-setting-btn"><i class="icon-settings"></i></button>
+				</div>
+			</div>
 		</div>
+		<!-- <div class="arrow-down">
+			<svg version="1.0" viewBox="0 0 32 32" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+				<polygon points="0.645,10.383 16,23.871 31.355,10.383 29.375,8.129 16,19.878 2.625,8.129 "></polygon>
+			</svg>
+		</div> -->
 	</header>
 
-	<div class="pt-55 pb-4"></div>
-
-	<main class="container-fluid px-lg-4 px-xl-5">
-		<section>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis cumque omnis sint quam deleniti perferendis inventore numquam ipsum laudantium ullam saepe culpa, soluta maiores voluptatum cum explicabo qui quod optio!</section>
+	<main>
+		<div id="result" class="p-5" style="display: none;">
+			<h1>Lorem Ipsum</h1>
+			<hr>
+			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta obcaecati deleniti, voluptate, impedit atque aut sint itaque, enim minus eligendi suscipit adipisci? Nisi id accusantium asperiores amet ducimus voluptate illo.</p>
+			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta obcaecati deleniti, voluptate, impedit atque aut sint itaque, enim minus eligendi suscipit adipisci? Nisi id accusantium asperiores amet ducimus voluptate illo.</p>
+			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta obcaecati deleniti, voluptate, impedit atque aut sint itaque, enim minus eligendi suscipit adipisci? Nisi id accusantium asperiores amet ducimus voluptate illo.</p>
+			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta obcaecati deleniti, voluptate, impedit atque aut sint itaque, enim minus eligendi suscipit adipisci? Nisi id accusantium asperiores amet ducimus voluptate illo.</p>
+			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta obcaecati deleniti, voluptate, impedit atque aut sint itaque, enim minus eligendi suscipit adipisci? Nisi id accusantium asperiores amet ducimus voluptate illo.</p>
+			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta obcaecati deleniti, voluptate, impedit atque aut sint itaque, enim minus eligendi suscipit adipisci? Nisi id accusantium asperiores amet ducimus voluptate illo.</p>
+			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta obcaecati deleniti, voluptate, impedit atque aut sint itaque, enim minus eligendi suscipit adipisci? Nisi id accusantium asperiores amet ducimus voluptate illo.</p>
+			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta obcaecati deleniti, voluptate, impedit atque aut sint itaque, enim minus eligendi suscipit adipisci? Nisi id accusantium asperiores amet ducimus voluptate illo.</p>
+			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta obcaecati deleniti, voluptate, impedit atque aut sint itaque, enim minus eligendi suscipit adipisci? Nisi id accusantium asperiores amet ducimus voluptate illo.</p>
+			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta obcaecati deleniti, voluptate, impedit atque aut sint itaque, enim minus eligendi suscipit adipisci? Nisi id accusantium asperiores amet ducimus voluptate illo.</p>
+		</div>
 	</main>
 
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.2/js/bootstrap-select.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.20/js/jquery.dataTables.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.20/js/dataTables.bootstrap4.min.js"></script>
@@ -43,6 +72,17 @@
 	<script src="assets/js/main.js?v=<?php include 'views/partials/_version.php'; ?>"></script>
 	<script>
 		const DEV = <?= json_encode(SITE === 'http://localhost') ?>;
+	</script>
+
+	<script>
+		$(() => {
+			$('#search').keypress(function(e) {
+				if (e.which === 13) {
+					$('body').toggleClass('search-active');
+					$('#result').slideToggle();
+				}
+			});
+		});
 	</script>
 
 </body>
