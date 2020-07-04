b40044547d09a76d7fb9b2ce9e3c3dd03fbdbfe8
Muhammad Afifudin <afif.x.udin@gmail.com>
2020-07-05 06:27:37 +0700

    Change PHP to HTML (for GitHub pages)

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('SITE', 'http://'.$_SERVER['HTTP_HOST']);
-// define('SITE', 'http://localhost');
-// define('SITE', 'https://webapps.bps.go.id/kayongutarakab');
-
-ini_set('max_execution_time', 40*60);
-
-error_reporting(E_ALL & ~E_NOTICE);
