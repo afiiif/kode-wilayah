61f782c6d74f9481061d8dbd4438413e9f6afc90
Muhammad Afifudin <afif.x.udin@gmail.com>
2020-07-19 22:22:00 +0700

    Remove unused Atlantis styles | Rename asset.src to src for naming convention

--- gulpfile.js
+++ gulpfile.js
@@ -9,23 +9,23 @@ const sourcemaps = require('gulp-sourcemaps');
 // Compile Sass
 function css_main() {
 	return gulp
-		.src('assets.src/scss/main.scss')
+		.src('src/scss/main.scss')
 		.pipe(sourcemaps.init())
-		.pipe(sass({ outputStyle: 'compressed', includePaths: ['./assets.src/scss'] }).on('error', sass.logError))
+		.pipe(sass({ outputStyle: 'compressed', includePaths: ['./src/scss'] }).on('error', sass.logError))
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('assets/css'));
 }
 function css_main_min() {
 	return gulp
-		.src('assets.src/scss/main.min.scss')
-		.pipe(sass({ outputStyle: 'compressed', includePaths: ['./assets.src/scss'] }).on('error', sass.logError))
+		.src('src/scss/main.min.scss')
+		.pipe(sass({ outputStyle: 'compressed', includePaths: ['./src/scss'] }).on('error', sass.logError))
 		.pipe(gulp.dest('assets/css'));
 }
 
 // Concat & minify JS
 function js_main() {
 	return gulp
-		.src(['assets.src/js/utilities.js', 'assets.src/js/main/**/*.js'])
+		.src(['src/js/utilities.js', 'src/js/main/**/*.js'])
 		.pipe(sourcemaps.init())
 		.pipe(concat('main.js'))
 		.pipe(babel({ presets: ['@babel/env'] }).on('error', function (e) { console.log(e) }))
@@ -35,7 +35,7 @@ function js_main() {
 }
 function js_main_min() {
 	return gulp
-		.src(['assets.src/js/utilities.js', 'assets.src/js/main/**/*.js'])
+		.src(['src/js/utilities.js', 'src/js/main/**/*.js'])
 		.pipe(concat('main.min.js'))
 		.pipe(babel({ presets: ['@babel/env'] }).on('error', function (e) { console.log(e) }))
 		.pipe(uglify())
@@ -45,9 +45,9 @@ function js_main_min() {
 
 // Watch files
 function watchFiles() {
-	gulp.watch('assets.src/scss/**/*.scss', gulp.series('css_main'));
-	gulp.watch('assets.src/js/main/**/*.js', gulp.series('js_main'));
-	gulp.watch('assets.src/js/utilities.js', gulp.series('js_main'));
+	gulp.watch('src/scss/**/*.scss', gulp.series('css_main'));
+	gulp.watch('src/js/main/**/*.js', gulp.series('js_main'));
+	gulp.watch('src/js/utilities.js', gulp.series('js_main'));
 }
 
 
